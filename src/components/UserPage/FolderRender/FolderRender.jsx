f8a475c19dc33b79f1a6f7bb2b3a76883d0586c2
Cameron Soudbash <csoudbash@gmail.com>
2022-03-25 16:31:58 -0500

    added accordians and started conditional rendering

--- src/components/UserPage/FolderRender/FolderRender.jsx
+++ src/components/UserPage/FolderRender/FolderRender.jsx
@@ -1,12 +1,26 @@
 import { useSelector } from 'react-redux';
+import Typography from '@mui/material/Typography';
+import React from 'react';
+
+import './FolderRender.css';
+import FolderDropdown from '../FolderDropdown/FolderDropDown';
 
 function FolderRender() {
 
+    const folders = useSelector((store) => store.folders.FoldersReducer);
+    // console.log("in folder render:", folders);
 
-
-
-    // return(
-
-    // )
+    return (
+        <>
+            <div className = 'folder-display'>
+                {folders.map((folder, i) => (
+                    <FolderDropdown
+                        key={i}
+                        folder={folder}
+                    />
+                ))}
+            </div>
+        </>
+    )
 }
 export default FolderRender;
